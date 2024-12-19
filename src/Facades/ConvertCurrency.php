b1210848e7f2d564f3a82e20353f6576a7b6f71f
kiyatilahun <98279891+KiyaTilahun@users.noreply.github.com>
2024-12-19 14:38:36 +0300

    Update ConvertCurrency.php

--- src/Facades/ConvertCurrency.php
+++ src/Facades/ConvertCurrency.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Kiyatilahun\ConvertCurrency\ConvertCurrency
+ * @method static \Illuminate\Http\JsonResponse convertAmount(float $amount, string $fromCurrency, string $toCurrency)
+ * @method static \Illuminate\Http\JsonResponse getAllCurrenciesSymbol()
+ * @method static \Illuminate\Http\JsonResponse getExchangeRate(string $fromCurrency,string $tocurrency)
+ * @method static \Illuminate\Http\JsonResponse getBirrExchangeRate(string $toCurrency)
  */
 class ConvertCurrency extends Facade
 {
